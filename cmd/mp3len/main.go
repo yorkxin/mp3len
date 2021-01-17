4a5de027fea99cbe343b2e9f0e752a0903ea19dc
Yucheng Chuang <ducksteven@gmail.com>
2021-01-17 20:51:41 +0900

    Properly handle resp.Body.Close() when no more to read

--- cmd/mp3len/main.go
+++ cmd/mp3len/main.go
@@ -8,8 +8,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"strconv"
-	"strings"
+	"time"
 
 	"github.com/yorkxin/mp3len"
 )
@@ -40,79 +39,56 @@ func openHTTP(location *url.URL) (io.ReadCloser, int64, error) {
 		return nil, 0, err
 	}
 
-	// avoid reading the whole response from server
-	// NOTE: this depends on server implementation of Range request.
-	// TODO: find a better way to stream, rather than guessing length.
-	req.Header.Set("Range", "bytes=0-65600")
-
 	resp, err := http.DefaultClient.Do(req)
 
 	if err != nil {
 		return nil, 0, err
 	}
 
-	length := resp.ContentLength
+	return resp.Body, resp.ContentLength, nil
+}
 
-	if cr := resp.Header.Get("Content-Range"); cr != "" {
-		// TODO: handle unit != "bytes"
-		// TODO: handle unknown size
-		splitted := strings.SplitN(cr, "/", 2)
+func processInput(location *url.URL) (time.Duration, error) {
+	var r io.ReadCloser
+	var totalLength int64
+	var err error
 
-		if len(splitted) == 2 {
-			totalBytes, err := strconv.Atoi(splitted[1])
-			if err == nil {
-				length = int64(totalBytes)
-			}
-			if err != nil {
-				// edge case: Content-Range is "*" or something unparsable
-			}
-		} else {
-			// edge case: Content-Range is not in standard "unit start-end/total" foramt.
-			// fallback to non-range request
-		}
+	if location.Scheme == "http" || location.Scheme == "https" {
+		r, totalLength, err = openHTTP(location)
+	} else if location.Scheme == "file" || location.Scheme == "" {
+		r, totalLength, err = openFile(location)
+	} else {
+		return 0, errInvalidInput
 	}
 
-	return resp.Body, length, nil
+	if err != nil {
+		return 0, err
+	}
+
+	defer r.Close()
+
+	return mp3len.EstimateDuration(r, totalLength)
 }
 
 func main() {
 	pause := flag.Bool("pause", false, "press any key to exit; useful for checking IO activity.")
 	flag.Parse()
 
-	input, err := url.Parse(flag.Arg(0))
+	location, err := url.Parse(flag.Arg(0))
 
-	if input.Path == "" || err != nil {
+	if location.Path == "" || err != nil {
 		fmt.Fprintln(os.Stderr, errInvalidInput)
 		os.Exit(1)
 	}
 
-	var r io.ReadCloser
-	var totalLength int64
-
-	if input.Scheme == "http" || input.Scheme == "https" {
-		r, totalLength, err = openHTTP(input)
-	} else if input.Scheme == "file" || input.Scheme == "" {
-		r, totalLength, err = openFile(input)
-	} else {
-		fmt.Fprintln(os.Stderr, errInvalidInput)
-		os.Exit(1)
-	}
+	duration, err := processInput(location)
 
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	defer r.Close()
-
-	duration, err := mp3len.EstimateDuration(r, totalLength)
-
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("%s\n", duration)
+	fmt.Println(duration.String())
 
 	if *pause == true {
 		anyKeyReader := bufio.NewReader(os.Stdin)
